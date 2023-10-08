562dcd2b6f7e50e0fa7a486c1662bf2865cb8ee2
akifisitan <102677971+akifisitan@users.noreply.github.com>
2023-10-08 21:46:47 +0300

    Remove unnecessary load function

--- src/routes/su/clash-checker/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from "./$types";
-import { courseMap, days } from "./data";
-
-export const load: PageServerLoad = async () => {
-	return {
-		courseMap,
-		days
-	};
-};
--- src/routes/su/clash-checker/+page.svelte
+++ src/routes/su/clash-checker/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	import { createScheduleTable } from "./data";
+	import { createScheduleTable, courseMap, days } from "./data";
 	import { Input } from "$lib/components/ui/input";
-	import { Label } from "$lib/components/ui/label";
 	import { Button, buttonVariants } from "$lib/components/ui/button";
 	import { Textarea } from "$lib/components/ui/textarea";
 	import * as Dialog from "$lib/components/ui/dialog";
-	import type { PageData } from "./$types";
 	import { onMount } from "svelte";
 
-	export let data: PageData;
 	let scheduleTable = createScheduleTable();
 	let loaded = false;
 	let importDialogIsOpen = false;
@@ -19,6 +16,16 @@
 
 	$: if (loaded) {
 		refreshActiveProfiles(Object.keys(profiles));
+	}
+
+	function assignCrnToSlots(crn: string) {
+		const course = courseMap[crn];
+		course.slots.forEach((slot) => {
+			const split = slot.split(":");
+			const hour = Number(split[1]);
+			const day = Number(split[0]);
+			scheduleTable[hour][day].push(course.code);
+		});
 	}
 
 	function refreshActiveProfiles(profileList: string[]) {
@@ -35,25 +42,6 @@
 		}
 		scheduleTable = scheduleTable;
 		localStorage.setItem("clash-checker-profiles", JSON.stringify(profiles));
-	}
-
-	function handleRandomize() {
-		const numRows = scheduleTable.length;
-		const numCols = scheduleTable[0].length;
-		const randomRow = Math.floor(Math.random() * numRows);
-		const randomCol = Math.floor(Math.random() * numCols);
-		scheduleTable[randomRow][randomCol].push("CS201 - A");
-		scheduleTable = scheduleTable;
-	}
-
-	function assignCrnToSlots(crn: string) {
-		const course = data.courseMap[crn];
-		course.slots.forEach((slot) => {
-			const split = slot.split(":");
-			const hour = Number(split[1]);
-			const day = Number(split[0]);
-			scheduleTable[hour][day].push(course.code);
-		});
 	}
 
 	function handleImport() {
@@ -73,7 +61,6 @@
 
 	onMount(() => {
 		const storedProfiles = localStorage.getItem("clash-checker-profiles");
-		console.log(storedProfiles);
 		if (storedProfiles) {
 			profiles = JSON.parse(storedProfiles);
 		}
@@ -134,7 +121,7 @@
 				</Dialog.Header>
 				<Dialog.Footer>
 					<Button
-						variant={"destructive"}
+						variant="destructive"
 						on:click={() => {
 							profiles = {};
 							confirmDialogIsOpen = false;
@@ -167,7 +154,7 @@
 		<table>
 			<thead>
 				<th />
-				{#each data.days as day}
+				{#each days as day}
 					<th>{day}</th>
 				{/each}
 			</thead>
@@ -178,8 +165,8 @@
 						{#each row as _, j}
 							<td
 								class="cell"
-								class:red={scheduleTable[i][j].length > 1}
-								class:green={scheduleTable[i][j].length == 0}
+								class:bg-red-700={scheduleTable[i][j].length > 1}
+								class:bg-emerald-700={scheduleTable[i][j].length == 0}
 							>
 								{#each scheduleTable[i][j] as courseCode}
 									<p class="text-center">{courseCode}</p>
@@ -195,16 +182,10 @@
 
 <style>
 	.cell {
-		@apply border-[2px] border-spacing-4;
+		border-width: 2px;
 		width: 2.5rem;
 		min-width: 10rem;
 		height: 2.5rem;
 		min-height: 2.5rem;
-	}
-	.red {
-		@apply bg-red-700;
-	}
-	.green {
-		@apply bg-emerald-700;
 	}
 </style>

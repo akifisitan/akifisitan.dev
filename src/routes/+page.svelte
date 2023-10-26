3fa6f58fc36f0299bd9320b659470e4779b36aaa
akifisitan <102677971+akifisitan@users.noreply.github.com>
2023-10-26 18:54:01 +0300

    accessibility and seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,13 @@
 	import { Linkedin, Github, FileText } from "lucide-svelte";
 </script>
 
-<svelte:head>
-	<title>akifisitan</title>
-</svelte:head>
-
 <div class="flex flex-col items-center justify-center w-full min-h-[100vh]">
 	<div class="flex flex-col w-72">
 		<div class="align-middle text-5xl pb-4">
 			<h1>Hey, I'm Akif</h1>
 		</div>
 		<div>
-			<p class="pb-2 text-center">
-				I am interested in <span class="text-violet-600">building software</span>
-			</p>
+			<p class="pb-2 text-center">I am interested in building software</p>
 			<div class="project-links">
 				<a
 					class="text-center bg-rose-800 border-[2px] border-gray-900 p-1 rounded-md hover:bg-gray-900"
@@ -34,13 +28,19 @@
 			<a
 				class="m-2 border-[3px] border-gray-600 rounded-sm"
 				href="https://github.com/akifisitan"
-				target="_blank"><Github class="bg-gray-600 hover:text-gray-900" /></a
+				target="_blank"
 			>
+				<Github class="bg-gray-600 hover:text-gray-900" />
+				<span class="sr-only">Github</span>
+			</a>
 			<a
 				class="m-2 border-[3px] border-blue-500 rounded-sm"
 				href="https://linkedin.com/in/akifisitan"
-				target="_blank"><Linkedin class="bg-blue-500 hover:text-blue-900" /></a
+				target="_blank"
 			>
+				<Linkedin class="bg-blue-500 hover:text-blue-900" />
+				<span class="sr-only">LinkedIn</span>
+			</a>
 			<!-- <a class="p-2" href="/files/cv" target="_blank"><FileText class="w-6 h-6" /></a> -->
 		</div>
 	</div>
